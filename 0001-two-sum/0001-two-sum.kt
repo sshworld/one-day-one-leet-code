312140a205d1e5de49ca56bfd4e1e093c012299e
SSH._.WORLD <ssh97540@gmail.com>
2023-11-09 23:16:03 +0900

    Time: 264 ms (24.69%), Space: 37.9 MB (83.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0..(nums.size - 2)) {
-            for (j in (i + 1)..(nums.size - 1)) {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
                 if (nums[i] + nums[j] == target) {
                     return intArrayOf(i, j)
                 }
