fb52773d1a6c1ea8cd8cca3307b170e418d48d6a
SSH._.WORLD <ssh97540@gmail.com>
2023-12-06 00:27:47 +0900

    Time: 174 ms (8.70%), Space: 33.3 MB (52.17%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.kt
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.kt
@@ -1,26 +1,5 @@
 class Solution {
     fun numberOfMatches(n: Int): Int {
-        var answer = 0
-        var teams = n
-        
-        while(teams != 1) {
-            if (teams % 2 == 0) {
-                answer += matchEven(teams)
-                teams = teams / 2
-            } else {
-                answer += matchOdd(teams)
-                teams = teams / 2 + 1
-            }
-        }
-        
-        return answer
-    }
-    
-    private fun matchOdd(teams: Int): Int {
-        return (teams - 1) / 2
-    }
-    
-    private fun matchEven(teams: Int): Int {
-        return teams / 2
+        return n - 1
     }
 }
