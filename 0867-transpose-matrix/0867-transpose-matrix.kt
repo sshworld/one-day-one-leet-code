310f7208bcc65c6d6fbe47912e6134d194271e1b
SSH._.WORLD <ssh97540@gmail.com>
2023-12-11 17:57:33 +0900

    Time: 234 ms (12.50%), Space: 38.4 MB (95.00%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.kt
+++ 0867-transpose-matrix/0867-transpose-matrix.kt
@@ -1,10 +1,8 @@
 class Solution {
     fun transpose(matrix: Array<IntArray>): Array<IntArray> {
-        return Array<IntArray>(matrix[0].size) { index ->
-            IntArray(matrix.size).apply {
-                matrix.forEachIndexed { i, num ->
-                    this[i] = num[index]
-                }
+        return Array<IntArray>(matrix[0].size) { i ->
+            IntArray(matrix.size) { j ->
+                matrix[j][i]
             }
         }
     }
