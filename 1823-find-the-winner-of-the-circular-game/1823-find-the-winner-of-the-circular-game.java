211a608e31fc5b33ed20c3816a23946a47948ff8
SSH._.WORLD <ssh97540@gmail.com>
2024-07-08 14:58:46 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (82.80%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        int result = 0;
+        
+        for(int i = 2; i <= n; i++) {
+            result = (result + k) % i;
+        }
+        
+        return result + 1;
+    }
+}
